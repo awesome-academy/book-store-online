48f6c2b40215e1796c1c798e84222490daf6d5bc
nguyenvandung <dungnguyen16011997@gmail.com>
2020-08-26 13:30:28 +0700

    sign_up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ApplicationRecord
+  USERS_PARAMS = %i(name email password password_confirmation).freeze
+
   validates :name, presence: true,
-  length: {maximum: Settings.validate.user.max_length_name}
+    length: {maximum: Settings.validate.user.max_length_name}
   validates :email, presence: true,
     length: {maximum: Settings.validate.user.max_length_email},
     format: {with: Settings.validate.user.VALID_EMAIL_REGEX},
     uniqueness: true
+  validates :password, presence: true,
+    length: {minimum: Settings.validate.user.min_length_password},
+    allow_nil: true
+
+  before_save :downcase_email
 
   has_secure_password
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
     root "static_pages#home"
 
     get "home", to: "static_pages#home"
+
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+
+    resources :users
   end
 end
